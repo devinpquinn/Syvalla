563f6897be792953c31867d464eef86cdc13d3ca
Devin <devinpquinn4@gmail.com>
2023-04-27 22:16:25 -0400

    simple main menu

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class MainMenuManager : MonoBehaviour
 {
     public Texture2D pointer;
+    public GameObject options;
 
     private void Start()
     {
         Cursor.SetCursor(pointer, new Vector2(0, 0), CursorMode.Auto);
+    }
+
+    public void ShowOptions()
+    {
+        options.SetActive(true);
+    }
+
+    public void HideOptions()
+    {
+        options.SetActive(false);
     }
 }
