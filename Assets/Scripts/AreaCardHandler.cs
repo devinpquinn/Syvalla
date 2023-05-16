4c5ca01337a264047f2bb432d19df3dbf1c718b5
Devin <devinpquinn4@gmail.com>
2023-05-16 19:56:12 -0400

    working on area cards

---
+++ Assets/Scripts/AreaCardHandler.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaCardHandler : MonoBehaviour
+{
+    public void StartCard()
+    {
+        //set audio volume to 0 and fade in
+        //lock player
+    }
+
+    public void EndCard()
+    {
+        //unlock player
+    }
+}
